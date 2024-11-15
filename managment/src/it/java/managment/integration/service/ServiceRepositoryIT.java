25507d2cfd98a4896af0b671c6ecdd70b7ef3470
Christian Mancini <mancinichristian00@gmail.com>
2024-11-15 11:41:10 +0100

    Added addClient IT
    Signed-off-by: Christian Mancini <mancinichristian00@gmail.com>

--- managment/src/it/java/managment/integration/service/ServiceRepositoryIT.java
+++ managment/src/it/java/managment/integration/service/ServiceRepositoryIT.java
@@ -1,6 +1,13 @@
 package managment.integration.service;
 
-import java.security.PublicKey;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.List;
 import java.util.Properties;
 
 import org.hibernate.SessionFactory;
@@ -8,11 +15,13 @@ import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import managment.model.Client;
 import managment.repository.client.ClientRepository;
 import managment.repository.client.ClientRepositoryHibernate;
 import managment.repository.purchase.PurchaseRepository;
@@ -24,9 +33,11 @@ import org.junit.jupiter.api.Test;
 @Testcontainers
 class ServiceRepositoryIT {
 	
+	private static String mysqlVersion = System.getProperty("mysql.version", "9.1.0");
+	
 	@Container
 	@SuppressWarnings({ "rawtypes", "resource" })
-	public static final MySQLContainer mysql = new MySQLContainer(DockerImageName.parse("mysql:9.1.0"))
+	public static final MySQLContainer mysql = new MySQLContainer(DockerImageName.parse("mysql:" + mysqlVersion))
 			.withDatabaseName("it-db")
 			.withUsername("manager")
 			.withPassword("it");
@@ -35,7 +46,7 @@ class ServiceRepositoryIT {
 
 	private ClientRepository clientRepository;
 
-	private PurchaseRepositoryHibernate purchaseRepository;
+	private PurchaseRepository purchaseRepository;
 
 	private PurchaseManagmentService service;
 	
@@ -66,7 +77,27 @@ class ServiceRepositoryIT {
 	}
 
 	@Test
-	void test() {
+	@DisplayName("Add Client should add a client to the database")
+	void testAddClient() {
+		Client client = new Client("existingClient");
+		addTestClientToDatabase(client);
+		Client toAdd = new Client("toAdd");
+		service.addClient(toAdd);
+		assertThat(readAllClientFromDatabase()).containsExactly(
+				new Client(1,"existingClient"),
+				new Client(2,"toAdd")
+				);
+		
+	}
+	
+	private List<Client> readAllClientFromDatabase() {
+		return sessionFactory
+				.fromTransaction(session -> session.createSelectionQuery("from Client", Client.class).getResultList());
+	}
+	
+	private void addTestClientToDatabase(Client client) {
+		sessionFactory.inTransaction(session ->
+			session.persist(client));
 	}
 
 }
