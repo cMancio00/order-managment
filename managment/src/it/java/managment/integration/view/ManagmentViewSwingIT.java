4905b326e02a6698b1d4221130cee5133576b033
Christian Mancini <mancinichristian00@gmail.com>
2024-11-26 12:07:56 +0100

    findAllClients
    Signed-off-by: Christian Mancini <mancinichristian00@gmail.com>

---
+++ managment/src/it/java/managment/integration/view/ManagmentViewSwingIT.java
@@ -0,0 +1,111 @@
+package managment.integration.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Properties;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import managment.controller.Managmentcontroller;
+import managment.model.Client;
+import managment.repository.client.ClientRepository;
+import managment.repository.client.ClientRepositoryHibernate;
+import managment.repository.purchase.PurchaseRepository;
+import managment.repository.purchase.PurchaseRepositoryHibernate;
+import managment.service.PurchaseManagmentService;
+import managment.view.swing.ManagmentViewSwing;
+
+@RunWith(GUITestRunner.class)
+public class ManagmentViewSwingIT extends AssertJSwingJUnitTestCase {
+
+	private static String mysqlVersion = System.getProperty("mysql.version", "9.1.0");
+
+	@ClassRule
+	@SuppressWarnings({ "rawtypes", "resource" })
+	public static final MySQLContainer mysql = (MySQLContainer) new MySQLContainer(DockerImageName.parse("mysql:" + mysqlVersion))
+			.withDatabaseName("ViewIT-db").withUsername("manager").withPassword("it").withReuse(true);
+
+	private static SessionFactory sessionFactory;
+	private ClientRepository clientRepository;
+	private PurchaseRepository purchaseRepository;
+	private PurchaseManagmentService service;
+	private Managmentcontroller controller;
+	private ManagmentViewSwing view;
+
+	private FrameFixture window;
+	
+	@BeforeClass
+	public static void setupServer() {
+		mysql.start();
+		Properties mysqlProperties = new Properties();
+		mysqlProperties.setProperty("hibernate.connection.url", mysql.getJdbcUrl());
+		mysqlProperties.setProperty("hibernate.connection.username", mysql.getUsername());
+		mysqlProperties.setProperty("hibernate.connection.password", mysql.getPassword());
+		System.setProperty("hibernate.connection.url", mysql.getJdbcUrl());
+		System.setProperty("hibernate.connection.username", mysql.getUsername());
+		System.setProperty("hibernate.connection.password", mysql.getPassword());
+		sessionFactory = new Configuration().setProperties(mysqlProperties).configure("hibernate-integration.cfg.xml")
+				.buildSessionFactory();
+	}
+	
+	@Before
+	public void setup() {
+		sessionFactory.getCache().evictAllRegions();
+		clientRepository = new ClientRepositoryHibernate();
+		purchaseRepository = new PurchaseRepositoryHibernate();
+		service = new PurchaseManagmentService(sessionFactory, clientRepository, purchaseRepository);
+	}
+	
+	@After
+	public void truncateSchema() {
+		sessionFactory.getSchemaManager().truncateMappedObjects();
+	}
+	
+	@Override
+	protected void onSetUp() throws Exception {
+		sessionFactory.getCache().evictAllRegions();
+		clientRepository = new ClientRepositoryHibernate();
+		purchaseRepository = new PurchaseRepositoryHibernate();
+		service = new PurchaseManagmentService(sessionFactory, clientRepository, purchaseRepository);
+		
+		GuiActionRunner.execute(() -> {
+		view = new ManagmentViewSwing();
+		controller =
+		new Managmentcontroller(view, service);
+		view.setManagmentController(controller);
+		return view;
+		});
+		window = new FrameFixture(robot(), view);
+		window.show(); // shows the frame to test
+	}
+	
+	@Test
+	public void testFindAllClients(){
+		Client firstClient = new Client("firstClient");
+		Client secondClient = new Client("secondClient");
+		service.addClient(firstClient);
+		service.addClient(secondClient);
+		
+		GuiActionRunner.execute(
+				() -> controller.findAllClients());
+		
+		assertThat(window.list("clientList").contents()).containsExactly(
+				new Client(1, "firstClient").toString(),
+				new Client(2, "secondClient").toString()
+				);
+	}
+
+}
