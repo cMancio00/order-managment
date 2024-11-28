fb156b170ecba12ad0233ecff83a407cc7b1f0da
Christian Mancini <mancinichristian00@gmail.com>
2024-11-28 15:00:09 +0100

    Groupped tests in ControllerService
    Signed-off-by: Christian Mancini <mancinichristian00@gmail.com>

--- managment/src/it/java/managment/integration/ControllerServiceIT.java
+++ managment/src/it/java/managment/integration/ControllerServiceIT.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -82,85 +83,92 @@ class ControllerServiceIT {
 	void tearDown() {
 		sessionFactory.getSchemaManager().truncateMappedObjects();
 	}
-
-	@Test
-	@DisplayName("Find all Clients when a client is present should return it as list")
-	void testFindAllClients() {
-		addClientToDatabase(new Client("firstClient"));
-		addClientToDatabase(new Client("secondClient"));
-		controller.findAllClients();
-		verify(view).showAllClients(asList(
-				new Client(1,"firstClient"),
-				new Client(2,"secondClient")));
-	}
 	
-	@Test
-	@DisplayName("Add client")
-	void testAddClient(){
-		Client toAdd = new Client("toAdd");
-		controller.add(toAdd);
-		verify(view).clientAdded(new Client(1, "toAdd"));
-	}
-	
-	@Test
-	@DisplayName("Remove client when existing")
-	void testRemoveClientWhenExisting(){
-		Client toDelete = new Client(1, "toDelete");
-		addClientToDatabase(new Client("toDelete"));
-		controller.remove(toDelete);
-		verify(view).clientRemoved(new Client(1, "toDelete"));
-	}
-	
-	@Test
-	@DisplayName("Find all purchase of an existing selected client")
-	void findAllPurchaseOfSelectedClient(){
-		Client selectedClient = new Client("selectedClient");
-		selectedClient.setPurchases(new ArrayList<Purchase>());
-		addClientToDatabase(selectedClient);
-		Purchase existingPurchase = new Purchase(TEST_DATE, 10.0);
-		existingPurchase.setClient(selectedClient);
-
-		sessionFactory.inTransaction(session -> {
-			Client client = session.find(Client.class, selectedClient.getId());
-			session.persist(existingPurchase);
-			client.getPurchases().add(existingPurchase);
-			session.merge(client);
-		});
+	@Nested
+	@DisplayName("Client")
+	class ClientTest{
+		@Test
+		@DisplayName("Find all Clients")
+		void testFindAllClients() {
+			addClientToDatabase(new Client("firstClient"));
+			addClientToDatabase(new Client("secondClient"));
+			controller.findAllClients();
+			verify(view).showAllClients(asList(
+					new Client(1,"firstClient"),
+					new Client(2,"secondClient")));
+		}
 		
-		controller.findAllPurchasesOf(selectedClient);
-		verify(view).showAllPurchases(asList(new Purchase(1, TEST_DATE, 10.0)));
-	}
-	
-	@Test
-	@DisplayName("Add purchase to selected client when exists")
-	void testAddPurchaseToSelectedClientWhenExists(){
-		Client selectedClient = new Client("selectedClient");
-		addClientToDatabase(selectedClient);
-		Purchase toAdd = new Purchase(TEST_DATE, 5.0);
+		@Test
+		@DisplayName("Add client")
+		void testAddClient(){
+			Client toAdd = new Client("toAdd");
+			controller.add(toAdd);
+			verify(view).clientAdded(new Client(1, "toAdd"));
+		}
 		
-		controller.addPurchaseToSelectedClient(
-				new Client(1, "selectedClient"), toAdd);
-		verify(view).purchaseAdded(new Purchase(1, TEST_DATE, 5.0));
+		@Test
+		@DisplayName("Remove client")
+		void testRemoveClientWhenExisting(){
+			Client toDelete = new Client(1, "toDelete");
+			addClientToDatabase(new Client("toDelete"));
+			controller.remove(toDelete);
+			verify(view).clientRemoved(new Client(1, "toDelete"));
+		}
+		
 	}
-	
-	@Test
-	@DisplayName("Remove purchase when exists")
-	void testRemovePurchaseWhenExists(){
-		Client selectedClient = new Client("selectedClient");
-		selectedClient.setPurchases(new ArrayList<Purchase>());
-		addClientToDatabase(selectedClient);
-		Purchase existingPurchase = new Purchase(TEST_DATE, 10.0);
-		existingPurchase.setClient(selectedClient);
 
-		sessionFactory.inTransaction(session -> {
-			Client client = session.find(Client.class, selectedClient.getId());
-			session.persist(existingPurchase);
-			client.getPurchases().add(existingPurchase);
-			session.merge(client);
-		});
-		
-		controller.remove(existingPurchase);
-		verify(view).purchaseRemoved(existingPurchase);
+	@Nested
+	@DisplayName("Purchase")
+	class PurchaseTest{
+		@Test
+		@DisplayName("Find all purchase of an existing selected client")
+		void findAllPurchaseOfSelectedClient(){
+			Client selectedClient = new Client("selectedClient");
+			selectedClient.setPurchases(new ArrayList<Purchase>());
+			addClientToDatabase(selectedClient);
+			Purchase existingPurchase = new Purchase(TEST_DATE, 10.0);
+			existingPurchase.setClient(selectedClient);
+			
+			sessionFactory.inTransaction(session -> {
+				Client client = session.find(Client.class, selectedClient.getId());
+				session.persist(existingPurchase);
+				client.getPurchases().add(existingPurchase);
+				session.merge(client);
+			});
+			
+			controller.findAllPurchasesOf(selectedClient);
+			verify(view).showAllPurchases(asList(new Purchase(1, TEST_DATE, 10.0)));
+		}
+		@Test
+		@DisplayName("Add purchase to selected client")
+		void testAddPurchaseToSelectedClientWhenExists(){
+			Client selectedClient = new Client("selectedClient");
+			addClientToDatabase(selectedClient);
+			Purchase toAdd = new Purchase(TEST_DATE, 5.0);
+			
+			controller.addPurchaseToSelectedClient(
+					new Client(1, "selectedClient"), toAdd);
+			verify(view).purchaseAdded(new Purchase(1, TEST_DATE, 5.0));
+		}
+		@Test
+		@DisplayName("Remove purchase")
+		void testRemovePurchaseWhenExists(){
+			Client selectedClient = new Client("selectedClient");
+			selectedClient.setPurchases(new ArrayList<Purchase>());
+			addClientToDatabase(selectedClient);
+			Purchase existingPurchase = new Purchase(TEST_DATE, 10.0);
+			existingPurchase.setClient(selectedClient);
+			
+			sessionFactory.inTransaction(session -> {
+				Client client = session.find(Client.class, selectedClient.getId());
+				session.persist(existingPurchase);
+				client.getPurchases().add(existingPurchase);
+				session.merge(client);
+			});
+			
+			controller.remove(existingPurchase);
+			verify(view).purchaseRemoved(existingPurchase);
+		}
 	}
 	
 	private void addClientToDatabase(Client client) {
